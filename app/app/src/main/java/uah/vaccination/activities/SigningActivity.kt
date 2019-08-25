1126f412e7ca379534d45fd0fd2f0e378b5bd086
Micheline Medhat <MichelineMedhat@gmail.com>
2019-08-25 21:26:59 +0200

    [APP] Add Signing Activity

---
+++ app/app/src/main/java/uah/vaccination/activities/SigningActivity.kt
@@ -0,0 +1,13 @@
+package uah.vaccination.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uah.vaccination.R
+
+class SigningActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signing)
+    }
+}
